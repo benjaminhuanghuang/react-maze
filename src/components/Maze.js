d0691bebe3696e731b487e9850cd73c7f0e94cee
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-18 16:41:28 -0700

    init ui

--- src/components/Maze.js
+++ src/components/Maze.js
@@ -5,22 +5,22 @@ class Maze extends Component {
   constructor(props) {
     super(props);
 
-    this.state ={
-      size:1,
+    this.state = {
+      size: 1,
       mazeData: {},
       running: false,
-      currentPos: [0,0]
+      currentPos: [0, 0]
     }
-  
+
     this.onInitClick = this.onInitClick.bind(this);
     this.onResolveClick = this.onResolveClick.bind(this);
-}
+    this.onInputChange = this.onInputChange.bind(this);
+  }
 
   initMaze() {
+    console.log("size", this.state.size);
     let newMaze = {};
-    let size = 0;
-
-    this.setState({mazeData:newMaze, size:size});
+    this.setState({mazeData:newMaze});
   }
 
   resolveMaze()
@@ -36,6 +36,10 @@ class Maze extends Component {
     return <h1>Test</h1>;
   }
 
+  onInputChange(event) {
+    this.setState({ size: event.target.value });
+  }
+  
   onInitClick()
   {
     console.log("onInitClick ", this.state);
@@ -51,14 +55,15 @@ class Maze extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <div className="col s2">
-            <input type="number" ref="input_size" />
+        <div style={{display:'flex', justifyContent:'center', alignItems:"center"}}>
+          <div className="input-field" style={{flex:"1", marginRight:"1em"}}>
+            <input id="size" type="number" min="1" max="100" value={this.state.size} onChange={this.onInputChange}/>
+            <label htmlFor="size" className="active">Maze size:</label>
           </div>
-          <div className="col s2">
+          <div style={{flex:"1"}}>
             <button className="waves-effect waves-light btn" onClick={this.onInitClick}>Init</button>
           </div>
-          <div className="col s2">
+          <div style={{flex:"5"}}>
             <button className="waves-effect waves-light btn" onClick={this.onResolveClick}>Resolve</button>
           </div>
         </div>
